418b18e5492981c8b71d586b73e10685e5a5c5cf
Peter Perez <peterperez@Peters-MacBook-Pro.local>
2020-06-26 14:34:50 +0100

    Start Homepage implementation

---
+++ resources/views/welcome-page.blade.php
@@ -0,0 +1,154 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>YoFamous</title>
+	<!-- <link rel="stylesheet" type="text/css" href="{{ asset('/assets/css/bootstrap.min.css') }}">
+	 -->
+	 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+
+
+	<!-- <script type="text/javascript" src="{{ asset('/assets/js/bootstrap.min.js') }}"></script> -->
+
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+</head>
+<body>
+
+	<section class="heroSection">
+		<div class="container">
+			<nav class="navbar navbar-expand-lg">
+				<a class="navbar-brand" href="#">
+					<img src="{{ asset('/img/logo-smaller.png') }}" class="logo">
+				</a>
+
+			  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation" style="color: red !important;">
+			    <span class="navbar-toggler-icon"></span>
+			  </button>
+
+			  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+			    <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Home</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Fanlink</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Artiste Page</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Social Media Shop</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Register</a>
+			      </li>
+			      <li class="nav-item">
+			        <a class="nav-link" href="#">Login</a>
+			      </li>
+			    </ul>
+			  </div>
+			</nav>
+
+
+			<div class="content">
+				<span class="heroText">Royal Music Promotion on<br/>Spotify | Apple Music | Audiomack | SoundCloud</span>
+				<div style="height: 22px;"></div>
+				<div class="sub">
+					<span class="subtext">Created By Musicians For Musicians</span>
+				</div>
+			</div>
+		</div>
+		
+	</section>
+
+	<section class="promoteNow">
+		<div class="container">
+			<div style="height: 40px;"></div>
+			<div class="promoteNowText">PromoteNow</div>
+			<div style="height: 40px;"></div>
+			<div class="row">
+				<div class="col-md sCard">
+					<div class="serviceCard">
+						<div class="priceAvatar">
+							<span>Starting from<br/><b>$5/1000</b> plays</span>
+						</div>
+						<div class="content">
+							<div class="title">
+								Spotify Plays
+							</div>
+							<div class="list">
+								Eligible For Royalties<br />
+								Promote Multiple Tracks<br />
+								Market-Geo Targeting
+							</div>
+						</div>
+					</div>
+					<div style="height: 20px;"></div>
+				</div>
+				<div class="col-md sCard">
+					<div class="serviceCard">
+						<div class="priceAvatar">
+							<span>Starting from<br/><b>$5/1000</b> plays</span>
+						</div>
+						<div class="content">
+							<div class="title">
+								Apple Music Plays
+							</div>
+							<div class="list">
+								Eligible For Royalties<br />
+								Promote Multiple Tracks<br />
+								Market-Geo Targeting
+							</div>
+						</div>
+					</div>
+					<div style="height: 20px;"></div>
+				</div>
+				<div class="col-md sCard">
+					<div class="serviceCard">
+						<div class="priceAvatar">
+							<span>Starting from<br/><b>$5/1000</b> plays</span>
+						</div>
+						<div class="content">
+							<div class="title">
+								Audiomack Plays
+							</div>
+							<div class="list">
+								Eligible For Royalties<br />
+								Promote Multiple Tracks<br />
+								Market-Geo Targeting
+							</div>
+						</div>
+					</div>
+					<div style="height: 20px;"></div>
+				</div>
+				<div class="col-md sCard">
+					<div class="serviceCard">
+						<div class="priceAvatar">
+							<span>Starting from<br/><b>$5/1000</b> plays</span>
+						</div>
+						<div class="content">
+							<div class="title">
+								SoundCloud Plays
+							</div>
+							<div class="list">
+								Eligible For Royalties<br />
+								Promote Multiple Tracks<br />
+								Market-Geo Targeting
+							</div>
+						</div>
+					</div>
+					<div style="height: 20px;"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<div style="height: 400px;"></div>
+
+	
+
+</body>
+</html>
