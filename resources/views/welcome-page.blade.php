06abd38fc8034744f18a37cd65f392fb93bdb411
Peter Perez <peterperez@Peters-MacBook-Pro.local>
2020-06-28 04:36:16 +0100

    Add live order tracking

--- resources/views/welcome-page.blade.php
+++ resources/views/welcome-page.blade.php
@@ -254,6 +254,23 @@
 		</div>
 	</section>
 
+	 <section class="custom-social-proof">
+	    <div class="custom-notification">
+	      <div class="custom-notification-container">
+	        <div class="custom-notification-image-wrapper imagePadding">
+	          <img src="https://www.pngitem.com/pimgs/m/78-786293_1240-x-1240-0-avatar-profile-icon-png.png" style="width: 50px;">
+	        </div>
+	        <div class="custom-notification-content-wrapper">
+	          <p class="custom-notification-content">
+	            Mr.Nagarajan - UK<br>Purchased <b>20,000</b> Plays   
+	            <small>10 mins ago</small>
+	          </p>
+	        </div>
+	      </div>
+	      <div class="custom-close"></div>
+	    </div>
+	  </section>
+
 	<section class="footer">
 		<div style="height: 40px;"></div>
 		<div class="container">
@@ -310,6 +327,14 @@
 
 <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/2.0.2/anime.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+
+<script>
+	setInterval(function(){ $(".custom-social-proof").stop().slideToggle('slow'); }, 3000);
+	  $(".custom-close").click(function() {
+	    $(".custom-social-proof").stop().slideToggle('slow');
+	 });
+</script>
 
 <script>
 	AOS.init();
@@ -389,6 +414,7 @@
 	    duration: 500,
 	    delay: 500
 	  });
+
 </script>
 
 </body>
